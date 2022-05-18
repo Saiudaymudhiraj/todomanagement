fe6c47d9c57e54250e8ddc5e91018d8e062f87ba
sai uday <saiuday96400@gmail.com>
2022-05-18 12:41:26 +0530

    todomanagement

---
+++ src/test/java/com/fullstack/todomanagement/TodomanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.todomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
